9d72906744dfba5fba0ee99cb85b8a9dc9f4b209
xiaozou <l54754712@yahoo.com>
2024-04-13 20:25:51 +0800

    add update time lock

--- src/modules/treasure/treasure/treasure.service.ts
+++ src/modules/treasure/treasure/treasure.service.ts
@@ -1,6 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { RedisClientType } from 'redis';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Injectable()
 export class TreasureService {
   @Inject('REDIS_CLIENT')
@@ -10,9 +12,18 @@ export class TreasureService {
     return { treasure: treasureValue };
   }
   async addOne(): Promise<{ treasure: string }> {
-    let treasureValue = Number(await this.redisClient.get('treasure'));
-    treasureValue += 1;
-    await this.redisClient.set('treasure', treasureValue);
+    const updateTime: null | string = await this.redisClient.get('updateTime');
+    if (
+      updateTime !== null &&
+      Number(updateTime) >= Number(process.env.UPDATE_LOCK_TIME)
+    ) {
+      throw new HttpException('短時間內超過修改上限', HttpStatus.FORBIDDEN);
+    }
+    await this.redisClient.incr('treasure');
+    if (updateTime === null) {
+      await this.redisClient.setEx('updateTime', 10, '0');
+    }
+    await this.redisClient.incr('updateTime');
     const outputValue = await this.redisClient.get('treasure');
     return { treasure: outputValue };
   }

792fbc941d0b30e2f349432eafaeddf9c22f1422
xiaozou <l54754712@yahoo.com>
2024-04-14 13:13:14 +0800

    change modules dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RedisService } from './modules/redis/redis/redis.service';
-import { TreasureService } from './modules/treasure/treasure/treasure.service';
-import { TreasureController } from './modules/treasure/treasure/treasure.controller';
+import { RedisConfig } from './modules/redis/redis.config';
+import { RedisService } from './modules/redis/redis.service';
+import { TreasureService } from './modules/treasure/treasure.service';
+import { TreasureController } from './modules/treasure/treasure.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, TreasureController],
-  providers: [AppService, RedisService, TreasureService],
+  providers: [AppService, RedisConfig, TreasureService, RedisService],
 })
 export class AppModule {}
